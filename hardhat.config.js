5143c7d0c2965b95f77e04729fd1f3ae61f58952
Addis Belete <addis.bel@gmail.com>
2022-02-21 15:51:10 +0300

    Put Private key in env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 const projectId = "2175d834e04d481eba6fccf6a5967e12"
-
+const fs = require("fs")
+privateKey = fs.readFileSync(".secret").toString();
 module.exports = {
 	solidity: "0.6.5",
 	defaultNetwork: "hardhat",
@@ -10,7 +11,7 @@ module.exports = {
 		},
 		rinkeby: {
 			URL: `https://rinkeby.infura.io/v3/${projectId}`,
-			accounts: ['0xcc1ef5a937b9414f226c3dab28184b9a44cb42d672cb32c1db223c0ab57c04c8'] //Private key of the account
+			accounts: ['privateKey'] //Private key of the account
 		}
 	}
 };
