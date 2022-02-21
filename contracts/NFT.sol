34d1ab4b1e9a760daf179e3e36f674beb71bd5bd
Addis Belete <addis.bel@gmail.com>
2022-02-21 17:08:45 +0300

    Call Counter library in NFT.sol

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,3 +3,10 @@ pragma solidity ^0.6.5;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "hardhat/console.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+}
