2d64c74b60e1494a7ab680c973e1fb54379d0830
Addis Belete <addis.bel@gmail.com>
2022-02-21 15:45:29 +0300

    Configure hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
-const fs = require('fs') //update the hradhat config with matic credentials
-const privateKey = fs.readFileSync(".secret").toString().trim();
-
+const projectId = "2175d834e04d481eba6fccf6a5967e12"
 
 module.exports = {
-	solidity: "0.8.4",
+	solidity: "0.6.5",
+	defaultNetwork: "hardhat",
+	networks: {
+		hardhat: {
+			chainID: 1337
+		},
+		rinkeby: {
+			URL: `https://rinkeby.infura.io/v3/${projectId}`,
+			accounts: ['0xcc1ef5a937b9414f226c3dab28184b9a44cb42d672cb32c1db223c0ab57c04c8'] //Private key of the account
+		}
+	}
 };
