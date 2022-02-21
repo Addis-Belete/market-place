ac798d8e21f92278535d78a963c4e4cda7fec542
Addis Belete <addis.bel@gmail.com>
2022-02-21 16:19:34 +0300

    Import openzeppelin to NFT.sol

---
+++ contracts/NFT.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.6.5;
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
