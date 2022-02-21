31da811eb063f322bba73687dceecc1a7cf0d977
Addis Belete <addis.bel@gmail.com>
2022-02-21 15:57:28 +0300

    Compile greeter.sol

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 const projectId = "2175d834e04d481eba6fccf6a5967e12"
 const fs = require("fs")
-privateKey = fs.readFileSync(".secret").toString();
+privateKey = fs.readFileSync(".secret").toString().trim()
 module.exports = {
 	solidity: "0.6.5",
 	defaultNetwork: "hardhat",
@@ -10,8 +10,8 @@ module.exports = {
 			chainID: 1337
 		},
 		rinkeby: {
-			URL: `https://rinkeby.infura.io/v3/${projectId}`,
-			accounts: ['privateKey'] //Private key of the account
+			url: `https://rinkeby.infura.io/v3/${projectId}`,
+			accounts: [privateKey] //Private key of the account
 		}
 	}
 };
