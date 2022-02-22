3caab1546bb86a760b6cb41c07425ab0f6382f0d
Addis Belete <addis.bel@gmail.com>
2022-02-22 11:31:57 +0300

    Add NFTMarket contract

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -6,3 +6,15 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import "hardhat/console.sol";
+
+contract NFTMarket is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemsIds;
+    Counters.Counter private _itemsSold;
+    address payable owner;
+    uint256 listingPrice = 0.02 ether;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+}
