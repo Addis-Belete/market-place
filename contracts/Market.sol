b4ce6f89fc8b86bfc06ce6a2d08eb654a1e49c7d
Addis Belete <addis.bel@gmail.com>
2022-02-23 19:46:11 +0300

    Add fetchItmesCreated function

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -137,4 +137,27 @@ contract NFTMarket is ReentrancyGuard {
         }
         return items;
     }
+
+    // Returns only Items a user has created
+
+    function fetchItemsCreated() public view returns (MarketItem[] memory) {
+        uint256 totalItemCount = _itemsIds.current();
+        uint256 itemCount = 0;
+        uint256 currentIndex = 0;
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i + 1].seller == msg.sender) {
+                itemcount += 1;
+            }
+        }
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i + 1].seller == msg.sender) {
+                uint256 currentId = i + 1;
+                MarketItem[] storage current = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
+    }
 }
