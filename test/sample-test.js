9e6db075170623609feba849310cd66281686bab
Addis Belete <addis.bel@gmail.com>
2022-02-23 21:02:17 +0300

    Add tests

--- test/sample-test.js
+++ test/sample-test.js
@@ -3,22 +3,47 @@ const { ethers } = require("hardhat");
 
 describe("NFTMarket", function () {
 	// deploy the marketplace
+	it("should create and execute market sales", async function () {
+		const Market = await ethers.getContractFactory("NFTMarket")
+		const market = await Market.deploy();
+		await market.deployed()
+		const marketAddress = market.address
 
-	const Market = await ethers.getContractFactory("NFTMarket")
-	const market = await Market.deploy();
-	await market.deployed()
-	const marketAddress = market.address
+		// deploy  the NFT contract
 
-	// deploy  the NFT contract
+		const NFT = await ethers.getContractFactory("NFT");
+		const nft = await NFT.deploy()
+		await nft.deployed();
+		const nftContractAddress = nft.address
+		let listingPrice = await market.getListingPrice();
+		listingPrice = listingPrice.toString()
 
-	const NFT = await ethers.getContractFactory("NFT");
-	const nft = await NFT.deploy()
-	await nft.deployed();
-	const nftContractAddress = nft.address
-	let listingPrice = await market.getListingPrice();
-	listingPrice = listingPrice.toString()
+		// create two tokens
+		await nft.createToken("https://www.mytokenlocation.com")
+		await nft.createToken("https://www.mytokenlocation2.com")
+		// put both token for sale
 
-	// create two tokens
-	await nft.createToken("https://www.mytokenlocation.com")
-	await nft.createToken("https://www.mytokenlocation2.com")
+		await market.createMarketItem(nftContractAddress, 1, auctionPrice, { value: listingPrice })
+		await market.createMarketItem(nftContractAddress, 2, auctionPrice, { value: listingPrice })
+
+		const [_, buyerAddress] = await ethers.getSigners()
+		// execute sale of token to another user
+		await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1, { value: auctionPrice })
+		// query for and return the unsold items
+
+		items = await market.fetchMarketItems()
+		items = await Promise.all(items.map(async i => {
+			const tokenUri = await nft.tokenURI(i.tokenId)
+			let item = {
+				price = i.price.toString(),
+				tokenId: i.tokenId.toString(),
+				seller: i.seller,
+				owner: i.owner,
+				tokenUri
+
+			}
+			return item
+		}))
+		console.log('items: ', items)
+	})
 })
