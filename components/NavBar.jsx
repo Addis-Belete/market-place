566ee1bf3fcbd8e7865c055ebd7906821a8b7a43
Addis Belete <addis.bel@gmail.com>
2022-02-25 13:06:46 +0300

    Add a Navbar component

---
+++ components/NavBar.jsx
@@ -0,0 +1,29 @@
+
+import Link from "next/link";
+const NavBar = () => {
+	return (
+		<nav className="border-b p-6 flex justify-between">
+				<p className="text-4xl font-bold">Addis's Marketplace</p>
+				<div className="flex mt-4">
+					<Link href="/">
+						<a className="mr-4 text-pink-500">Home</a>
+					</Link>
+
+					<Link href="/create-item">
+						<a className="mr-6 text-pink-500">Sell Digital Asset</a>
+					</Link>
+					<Link href="/my-assets">
+						<a className="mr-6 text-pink-500">My Digital Assets</a>
+					</Link>
+					<Link href="/creator-dashboard">
+						<a className="mr-6 text-pink-500">Creator Dashboard</a>
+					</Link>
+					<Link href="/creator-dashboard">
+						<a className="mr-6 text-pink-500">Connect</a>
+					</Link>
+				</div>
+			</nav>
+	)
+}
+
+export default NavBar
--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,10 @@
 import "../styles/globals.css";
-import Link from "next/link";
+import NavBar from "../components/NavBar";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<div>
-			<nav className="border-b p-6">
-				<p className="text-4xl font-bold">Addis's Marketplace</p>
-				<div className="flex mt-4">
-					<Link href="/">
-						<a className="mr-4 text-pink-500">Home</a>
-					</Link>
-
-					<Link href="/create-item">
-						<a className="mr-6 text-pink-500">Sell Digital Asset</a>
-					</Link>
-					<Link href="/my-assets">
-						<a className="mr-6 text-pink-500">My Digital Assets</a>
-					</Link>
-					<Link href="/creator-dashboard">
-						<a className="mr-6 text-pink-500">Creator Dashboard</a>
-					</Link>
-				</div>
-			</nav>
+			<NavBar />
 
 			<Component {...pageProps} />
 		</div>
