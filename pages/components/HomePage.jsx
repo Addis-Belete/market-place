829f783bb26eaa10dbf0fd382ab3b5b627a41723
Addis Belete <addis.bel@gmail.com>
2022-02-25 16:04:08 +0300

    Add loadNfts function

--- pages/components/HomePage.jsx
+++ pages/components/HomePage.jsx
@@ -6,8 +6,21 @@ import web3Modal from "web3modal"
 import {
 nftAddress, nftmarketAddress
 } from '../../config'
-
+import NFTABI from'../../artifacts/contracts/Market.sol/NFTMarket.json'
+import NFTMarketABI from '../../artifacts/contracts/NFT.sol/NFT.json'
 const HomePage = () => {
+
+async function loadNFTs(){
+const provider = new ethers.providers.JsonRpcProvider()
+const marketContract = new ethers.Contract(nftmarketAddress, NFTMarketABI, provider)
+const tokenContract = new ethers.Contract(nftAddress, NFTABI, provider)
+const data = await marketContract.fetchMarketItems()
+// map over itens returned from smart contract and 
+// format them as well as fetch their token metadata
+
+
+}
+
 	return (
 		<div>
 			
