241ff79a86fcbb117a4651a9f8654a89c82c8403
Addis Belete <addis.bel@gmail.com>
2022-02-26 14:39:22 +0300

    Add createMarket function

--- components/CreateNfts.jsx
+++ components/CreateNfts.jsx
@@ -9,22 +9,41 @@ import Market from "../artifacts/contracts/Market.sol/NFTMarket.json";
 const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
 
 const CreateNfts = () => {
-const [fileUrl, setFileUrl] = useState(null)
-const [formInput, setFormInput] = useState({price: '', name: '', description: '' })
-const router = useRouter()
+  const [fileUrl, setFileUrl] = useState(null);
+  const [formInput, setFormInput] = useState({
+    price: "",
+    name: "",
+    description: "",
+  });
+  const router = useRouter();
 
-async function onChange(e){
-const  file = e.target.files[0]
-try {
-const added = await client.add(file, {
-progress: (prog) => console.log(`received: ${prog}`)
+  async function onChange(e) {
+    const file = e.target.files[0];
+    try {
+      const added = await client.add(file, {
+        progress: (prog) => console.log(`received: ${prog}`),
+      });
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+      setFileUrl(url);
+    } catch (error) {
+      console.log("Error uploading file: ", error);
+    }
+  }
+async function createMarket(){
+const {name, description, price} = formInput
+if(!name || !description || !price || !fileUrl) return
+const data = JSON.stringify({
+name, description, image: fileUrl
+
 })
+try{
+const added = await client.add(data)
 const url = `https://ipfs.infura.io/ipfs/${added.path}`
-setFileUrl(url)
-} catch(error){
-console.log('Error uploading file: ', error)
-
+createSale(url)
+}catch(error){
+console.log(' Error uploading file: ', error)
 }
+
 
 }
   return (
@@ -51,7 +70,6 @@ console.log('Error uploading file: ', error)
         {fileUrl && <img className="rounded mt-4 " width="350" src={fileUrl} />}
 
         <button className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
-          
           Create Digital Asset
         </button>
       </div>
