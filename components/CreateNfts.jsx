1445685759ee79ab79af6cd91ef4f9fe3dcf64cb
Addis Belete <addis.bel@gmail.com>
2022-02-26 12:15:32 +0300

    Add a UI for Create item page

--- components/CreateNfts.jsx
+++ components/CreateNfts.jsx
@@ -1,19 +1,44 @@
-import { useState } from "react"
-import { ethers } from "ethers"
-import { create as ipfsHttpClient } from "ipfs-http-client"
-import { useRouter } from "next/router"
-import Web3Modal from 'web3modal'
-import { nftaddress, nftmarketaddress } from "../config"
-import NFT from'../artifacts/contracts/NFT.sol/NFT.json'
-import Market from '../artifacts/contracts/Market.sol/NFTMarket.json'
-const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+import { useState } from "react";
+import { ethers } from "ethers";
+import { create as ipfsHttpClient } from "ipfs-http-client";
+import { useRouter } from "next/router";
+import Web3Modal from "web3modal";
+import { nftaddress, nftmarketaddress } from "../config";
+import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
+import Market from "../artifacts/contracts/Market.sol/NFTMarket.json";
+const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
 
 const CreateNfts = () => {
-	return (
-		<div>
-		Hello	
-		</div>
-	)
-}
+  return (
+    <div className=" flex justify-center">
+      <div className="w-1/2 flex flex-col pb-12">
+        <input
+          type="text"
+          placeholder="Asset Name"
+          className="mt-8 border rounded p-4"
+        />
+        <textarea
+          name="description"
+          id="description"
+          cols="30"
+          rows="10"
+          placeholder="Asset Description"
+          className="mt-2 border rounded p-4"
+        />
+        <input
+          placeholder="Asset Price in ETH"
+          className="mt-2 border rounded p-4"
+        />
+        <input type="file" name="Asset" id="asset" className="my-4" />
+        {fileUrl && <img className="rounded mt-4 " width="350" src={fileUrl} />}
 
-export default CreateNfts
+        <button className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
+          
+          Create Digital Asset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CreateNfts;
