15af9c2f29f5ca71325a48442fe9520ebad1dcb1
Addis Belete <addis.bel@gmail.com>
2022-02-26 12:06:44 +0300

    Import addresses and abi

--- components/CreateNfts.jsx
+++ components/CreateNfts.jsx
@@ -3,6 +3,11 @@ import { ethers } from "ethers"
 import { create as ipfsHttpClient } from "ipfs-http-client"
 import { useRouter } from "next/router"
 import Web3Modal from 'web3modal'
+import { nftaddress, nftmarketaddress } from "../config"
+import NFT from'../artifacts/contracts/NFT.sol/NFT.json'
+import Market from '../artifacts/contracts/Market.sol/NFTMarket.json'
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+
 const CreateNfts = () => {
 	return (
 		<div>
