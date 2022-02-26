863cfd77cd542f598699d09c81cc5098ef4ddca7
Addis Belete <addis.bel@gmail.com>
2022-02-26 14:54:37 +0300

    Add createMarket Function

--- components/CreateNfts.jsx
+++ components/CreateNfts.jsx
@@ -29,23 +29,47 @@ const CreateNfts = () => {
       console.log("Error uploading file: ", error);
     }
   }
-async function createMarket(){
-const {name, description, price} = formInput
-if(!name || !description || !price || !fileUrl) return
-const data = JSON.stringify({
-name, description, image: fileUrl
+  async function createMarket() {
+    const { name, description, price } = formInput;
+    if (!name || !description || !price || !fileUrl) return;
+    const data = JSON.stringify({
+      name,
+      description,
+      image: fileUrl,
+    });
+    try {
+      const added = await client.add(data);
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+      createSale(url);
+    } catch (error) {
+      console.log(" Error uploading file: ", error);
+    }
+  }
+  async function createSale(url) {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    let contract = new ethers.Contract(nftaddress, NFT.abi, signer);
+    let transaction = await contract.createToken(url);
+    let tx = await transaction.wait();
+    console.log("tx: ", tx);
 
-})
-try{
-const added = await client.add(data)
-const url = `https://ipfs.infura.io/ipfs/${added.path}`
-createSale(url)
-}catch(error){
-console.log(' Error uploading file: ', error)
-}
+    let event = tx.events[0];
+    let value = event.args[2];
+    let tokenId = value.toNumber();
+    const price = ethers.utils.parseUnits(formInput.price, "ether");
 
+    contract = new ethers.Contract(nftmarketaddress, Market.abi, signer);
+    let listingPrice = await contract.getListingPrice();
+    listingPrice = listingPrice.toString();
+    transaction = await contract.createMarketItem(nftaddress, tokenId, price, {
+      value: listingPrice,
+    });
+    await transaction.wait();
+    router.push("/");
+  }
 
-}
   return (
     <div className=" flex justify-center">
       <div className="w-1/2 flex flex-col pb-12">
@@ -53,6 +77,7 @@ console.log(' Error uploading file: ', error)
           type="text"
           placeholder="Asset Name"
           className="mt-8 border rounded p-4"
+onChange={e => setFormInput({...formInput, name: e.target.value})}
         />
         <textarea
           name="description"
@@ -61,15 +86,17 @@ console.log(' Error uploading file: ', error)
           rows="10"
           placeholder="Asset Description"
           className="mt-2 border rounded p-4"
+onChange={e => setFormInput({...formInput, description: e.target.value})}
         />
         <input
           placeholder="Asset Price in ETH"
           className="mt-2 border rounded p-4"
+onChange={e => setFormInput({...formInput, price: e.target.value})}
         />
-        <input type="file" name="Asset" id="asset" className="my-4" />
+        <input type="file" name="Asset" id="asset" className="my-4" onChange={onChange}/>
         {fileUrl && <img className="rounded mt-4 " width="350" src={fileUrl} />}
 
-        <button className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
+        <button className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg" onClick={createMarket}>
           Create Digital Asset
         </button>
       </div>
