2dc3b2b8fd759fba56752dda5f8d69e28394f8f3
Addis Belete <addis.bel@gmail.com>
2022-02-28 09:36:47 +0300

    Import ABI and contract address

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
+import {ethers} from 'ethers'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import web3Modal from 'web3Modal'
 
+import { nftmarketaddress, nftaddress } from '../config'
+ import Market from '../artifacts/contracts/Market.sol/NFTMarket.json'
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
 const Dashboard = () => {
 	return (
 		<div>
