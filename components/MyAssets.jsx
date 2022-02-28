4e3fd63937d22157dee0c82193db6daaf9fd4f43
Addis Belete <addis.bel@gmail.com>
2022-02-28 09:02:32 +0300

    Add a loadNFts fuction that returns owned nfts

--- components/MyAssets.jsx
+++ components/MyAssets.jsx
@@ -8,6 +8,44 @@ import Market from '../artifacts/contracts/Market.sol/NFTMarket.json'
 import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
 
 const MyAssets = () => {
+const [nfts, setNfts] = useState([])
+const [loadingSate, setLoadingState] = useState('not-loaded')
+ useEffect(()=>{
+loadNFTS()
+
+}, [])
+
+async function loadNFTS(){
+const web3Modal = new Web3Modal({
+newtwork: "mainnet",
+cacheProvider: true
+})
+const connection = await web3Modal.connect()
+const provider = new ethers.providers.Web3Provider(connection)
+const signer = provider.getSigner()
+const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
+const data = await marketContract.fetchMyNFTs()
+
+const items = Promise.all(data.map(async i => {
+const tokenUri = await tokenContract.tokenURI(i.tokenId)
+const meta = await axios.get(tokenUri)
+let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+let item = {
+price,
+tokenId: i.tokenId.toNumber(),
+seller: i.seller,
+owner: i.owner,
+image: meta.data.image,
+
+}
+return item
+}))
+setNfts(items)
+setLoadingState('loaded')
+}
+if(loadingSate === 'loaded' && !nfts.length) return (<h1 className="py-10 px-20 text-3xl">No Assets owned</h1>)
+
 	return (
 		<div>
 			Hello World
