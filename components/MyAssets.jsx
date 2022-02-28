06b1dd7c634f0b001bb3b4a94d253ff2dde2bb8c
Addis Belete <addis.bel@gmail.com>
2022-02-28 08:46:23 +0300

    Import NFT and Market contracts ABI

--- components/MyAssets.jsx
+++ components/MyAssets.jsx
@@ -4,7 +4,8 @@ import axios from "axios"
 import Web3Modal from web3Modal
 
 import { nftaddress, nftmarketaddress } from "../config"
-
+import Market from '../artifacts/contracts/Market.sol/NFTMarket.json'
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
 
 const MyAssets = () => {
 	return (
